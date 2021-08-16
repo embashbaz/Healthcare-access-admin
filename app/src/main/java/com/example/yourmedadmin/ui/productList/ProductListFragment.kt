1a1c4f10cc22e23ac68f2cc9000b01d97e3bff30
Emery Bashige <bemeryb@gmail.com>
2021-08-16 17:14:07 +0300

    binded the views for the product list fragment

--- app/src/main/java/com/example/yourmedadmin/ui/productList/ProductListFragment.kt
+++ app/src/main/java/com/example/yourmedadmin/ui/productList/ProductListFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.yourmedadmin.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class ProductListFragment : Fragment() {
 
+    lateinit var listRecycler: RecyclerView
+    lateinit var noDataTxt: TextView
+    lateinit var addProductFb: FloatingActionButton
+    lateinit var loadingProgress: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +28,10 @@ class ProductListFragment : Fragment() {
     }
 
     fun bindViews(view: View){
-
+        listRecycler = view.findViewById(R.id.product_list_recycler)
+        noDataTxt = view.findViewById(R.id.no_data_product_list)
+        addProductFb= view.findViewById(R.id.add_product_fb)
+        loadingProgress = view.findViewById(R.id.progressBar_product_list)
     }
 
 }

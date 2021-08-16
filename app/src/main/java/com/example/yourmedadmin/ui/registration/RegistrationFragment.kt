b4e2d1ebc145068dd0e38ac3707180fc5d928711
Emery Bashige <bemeryb@gmail.com>
2021-08-16 18:18:19 +0300

    added the registration viewmodel and added the spinner arrays

--- app/src/main/java/com/example/yourmedadmin/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/com/example/yourmedadmin/ui/registration/RegistrationFragment.kt
@@ -36,7 +36,14 @@ class RegistrationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        val view = inflater.inflate(R.layout.fragment_registration, container, false)
+        bindViews(view)
+        registerBt.setOnClickListener{
+
+        }
+
+
+        return view
     }
 
     fun bindViews(view: View){
--- app/src/main/java/com/example/yourmedadmin/ui/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/example/yourmedadmin/ui/registration/RegistrationViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.yourmedadmin.ui.registration
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.yourmedadmin.data.CareAdmin
+import com.example.yourmedadmin.data.Repository
 
 class RegistrationViewModel  : ViewModel(){
+    val repository = Repository()
 
+
+    private var _registrationOutput = MutableLiveData<HashMap<String, String>>()
+    val registrationOutput: LiveData<HashMap<String, String>>
+        get() = _registrationOutput
+
+    fun signUp (medAdmin: CareAdmin, password: String){
+        _registrationOutput = repository.register(medAdmin, password)
+    }
 }
